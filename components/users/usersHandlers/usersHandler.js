1b94fa0e941577d402149c491d3544ff3ae52d99
martinriveros <martin.riveros@hotmail.com>
2022-01-25 20:17:48 -0300

    heroku

--- components/users/usersHandlers/usersHandler.js
+++ components/users/usersHandlers/usersHandler.js
@@ -1,64 +1,18 @@
-const usersModel = require("../../../config/db");;
-const cp = require('child_process')
-const path = require('path')
-const {calculateRandom} = require('../usersServices/calculateRandom')
+const cp =                    require('child_process')
+const path =                  require('path')
+const {calculateRandom} =     require('../usersServices/calculateRandom')
+const args =                  require('./utils/yargs')
+
 var calculateRandomCP
 var childProcessActive=false
 
+let {mode, port} = args
+
 class UsersHandler {
-    
-    async protectedContent(req, res) {
-      let user = await usersModel.findById(req.session.passport.user)
-      res.render("layouts/protectedContent", { loggedUser: user.name });
-    }
-
-  async userLoginForm(req, res) {
-    let user
-    try {
-      user = await usersModel.findById(req.session.passport.user)
-    } catch (error) {
-      console.log('no existe el usuario registrado aun')
-    }
-       
-  if (req.isAuthenticated()) {
-      if(req.comesFromRegistro===true){
-        return res.render("layouts/userLogin",{ loggedUser: user.name });
-      } else {
-        return res.redirect("/protectedContent");
-      }
-    } else {
-      return res.render("layouts/userLogin", { loggedUser: 'Gest' })
-    }}
-    
-
-  async userRegisterForm(req, res) {
-    let formErrors = req.formErrors
-    if (req.isAuthenticated()) {
-      return res.redirect("/protectedContent");
-    } else {
-      return res.render("layouts/userRegister", {formErrors});
-    }
-  }
-
-  async userFailLogin(req, res) {
-      res.send('fail login')
-  }
-  async userFailRegistro(req, res) {
-      res.send('fail registro')
-  }
-  async notMatchingRoute(req, res) {
-      res.send('el contenido al que estas tratando de llegar no existe')
-  }
-  async getWelcomePage(req, res, next) {
-    res.render("layouts/welcomePage");
-  }
-
-  async logout(req, res, next) {
-    res.logout();
-    res.render('/welcome')
-  }
+  
+  
   async getArgvInfo(req, res, next) {
-
+    
     const data = [
       
       {desc: 'Argumentos de entrada: ', value: process.argv.length === 2 ?  'no arguments on command line': process.argv[2]},
@@ -70,7 +24,8 @@ class UsersHandler {
       {desc: 'ID de Proceso: ', value:process.pid},
       {desc: 'Carpeta de Proyecto', value: process.cwd()},
     ];
-
+    
+    console.log('esto hay dentro de args', args)
     res.render('layouts/info', {data})
   }
 
@@ -82,7 +37,7 @@ class UsersHandler {
     
     console.log(process.argv[2])
     
-    if(process.argv[2]==='--fork' && !childProcessActive){
+    if(args.mode==='--fork' && !childProcessActive){
             
         calculateRandomCP = cp.fork(path.join(__dirname, '../usersServices/calculateRandom'))          
         
--- nuevo.js
+++
@@ -1,7 +0,0 @@
-console.time('one line execution time')
-
-let lalala = (function Now(){
-    console.log('lalala');
-}())
-console.log(typeof lalala)
-console.timeEnd('one line execution time')
